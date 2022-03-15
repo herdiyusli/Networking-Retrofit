4b2afec2fbd18b18cea7157968c95e2b2d1a80eb
herdi34 <herdi13yyo@gmail.com>
2022-03-15 17:03:07 +0700

    first commit

---
+++ app/src/main/java/com/herdi/yusli/restaurantreview/ApiService.kt
@@ -0,0 +1,11 @@
+package com.herdi.yusli.restaurantreview
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
